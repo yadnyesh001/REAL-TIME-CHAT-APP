7939afaadc37bf3620c59fe6aa232e6c3f538543
Yadnyesh001 <yadnyeshbadgujar2004@gmail.com>
2025-05-21 11:34:34 +0530

    authentication

--- backend/server.js
+++ backend/server.js
@@ -2,18 +2,22 @@ import express from 'express';
 import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.routes.js';
+import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+dotenv.config();
+
+app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// }); 
+
 app.listen(PORT, () => {
+  connectToMongoDB();
   console.log(`Server is running on port ${PORT}`);
 });
